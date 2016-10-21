45ed5e606174cf90d1dd1ed4dce08d3179a3d4d9
Eazuara <A01410412@itesm.mx>
2016-10-20 20:00:58 -0500

    Update FPP16.java

--- FP-P16/src/fp/p16/FPP16.java
+++ FP-P16/src/fp/p16/FPP16.java
@@ -16,14 +16,14 @@ public class FPP16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //DECLARACION DE VARIABLES PARA INTRODUCIR DATOS EN STRING
         int a,b,c;
         a=SolicitarDato(" a ");
         b=SolicitarDato(" b ");
         c=SolicitarDato(" c ");
         mostrarMayor(a,b,c);
     }
-    public static int SolicitarDato(String Dato){
+    public static int SolicitarDato(String Dato){//METODO DE PEDIR DATOS EN STRING
         int num;
         Scanner teclado=new Scanner(System.in);
         System.out.println("Ingrese numero"+ Dato);
@@ -31,13 +31,13 @@ public class FPP16 {
         return num;
         
     }
-    public static void mostrarMayor(int a,int b,int c){
+    public static void mostrarMayor(int a,int b,int c){//CONDICIONES Y CHECAR QUE NUMERO ES EL MAS GRANDE
         int m;
-        m=a;
-        if(m<b)m=b;
-        if(m<c)m=c;
+        m=a;//TOMAS EL PRIMER VALOR INGRESADO
+        if(m<b)m=b;//COMPARAS EL PRIMER VALOR CON EL SEGUNDO SI ES MENOR ATOMAS EL SEGUNDO
+        if(m<c)m=c;// COMPARAS CON EL TERCERO Y VES CUAL ES MAYOR
         
-        System.out.println("El numero mayor es"+" " + m);
+        System.out.println("El numero mayor es"+" " + m);//IMPRIMES
         
     }
     
